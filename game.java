c9f34a5d4bd86e314e32e846dd16f1e62c13f369
Philip Ayoub <PhilipAyoub@dhcp-vl2041-16339.eduroam.cornell.edu>
2021-11-05 15:02:20 -0400

    Game Updates

--- game.java
+++ game.java
@@ -1,6 +1,44 @@
-public game implements gameInterface{
+import gameInterface;
 
+public class game implements gameInterface {
+  public int[][] board = new int[6][7];
+  public int turn = 1;
+  public int yellow = 1;
+  public int red = -1;
+  public int id;
 
+  public game(int boardId) {
+    id = boardId;
+  }
 
+  public boolean gameOver() {
+    return true;
+  }
 
+  public void changeTurn() {
+    if (this.turn == -1)
+      this.turn = 1;
+    else
+      this.turn = -1;
+  }
+
+  public boolean placeMove(int colMove) {
+    for (int row = 5; row > -1; row--) {
+      if (this.board[row][colMove] == 0) {
+        this.board[row][colMove] = this.turn;
+        changeTurn();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public void resetBoard() {
+
+  }
+
+  public void showBoard(int[][] board) {
+
+  }
 }
--- gameInterface.java
+++ gameInterface.java
@@ -1,17 +1,22 @@
 public interface gameInterface {
-  // Represents the Physical Connect4 Board
-  public int[][] board = new int[6][7];
-  public int turn = 0;
+
+  public int[][] board; // Represents the Connect4 board
+  public int turn; // Variable the holds whose turn it is
+  public int yellow; // Player1
+  public int red; // Player2
+  public int id; // GameID
 
   // Checks to see if the game has been won/draw
-  public boolean gameOver(int Turn, int[][] board);
+  public boolean gameOver();
 
   // Places move in desired column
-  public int[][] placeMove(int column, int[][] board);
+  // Input: Numerical number indexed at 0
+  // Output: True if valid move, False Else
+  public boolean placeMove(int column);
 
   // Wipes the board of all pieces
-  public int[][] resetBoard();
+  public void resetBoard();
 
-  // Prints the board clearly to the terminal
+  //Prints the board clearly to the terminal
   public void showBoard(int[][] board);
 }
